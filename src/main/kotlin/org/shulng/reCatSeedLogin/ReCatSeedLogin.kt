5b9ae6a74da83f65797ef33233dc6a3e011be7af
myChinesePYL <3223285598@qq.com>
2024-08-13 17:20:50 +0800

    添加了个输出(qwq)

--- src/main/kotlin/org/shulng/reCatSeedLogin/ReCatSeedLogin.kt
+++ src/main/kotlin/org/shulng/reCatSeedLogin/ReCatSeedLogin.kt
@@ -8,9 +8,11 @@ class ReCatSeedLogin : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
         HandySchedulerUtil.init(this);
+        System.out.println("\033[38;2;0;255;0mLoaded RCSL\033[0m");
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        System.out.println("\033[38;2;255;0;0mStoped RCSL\033[0m");
     }
 }
